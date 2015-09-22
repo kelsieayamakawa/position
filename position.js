908696973e420fe88b920019287b4804813fd77f
Kelsie Yamakawa <kayama@hawaii.edu>
2015-09-21 16:11:10 -1000

    Finished initial version

---
+++ position.js
@@ -0,0 +1,23 @@
+/*
+ * Implement the position function described in:
+ * https://blog.svpino.com/2015/05/24/programming-challenge-the-position-of-the-element
+ * Created by Kelsie on 9/21/2015.
+ */
+
+function position(nums, num) {
+  var cursor = 0;
+  while ((nums[cursor] <= num) && (cursor < nums.length)) {
+    if (nums[cursor] === num) {
+      return cursor;
+    }
+    cursor++;
+  }
+  return cursor;
+}
+
+var testNums = [1, 3, 5, 6];
+
+console.log(position(testNums, 5));
+console.log(position(testNums, 2));
+console.log(position(testNums, 7));
+console.log(position(testNums, 0));
